237752003365ec3810a78880c803957db09c51e8
asolanky <alwinsolanky@gmail.com>
2018-01-14 18:14:08 +0000

    added second test

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -2,6 +2,10 @@ var Add = require("../app");
 
 describe("Add functionality", () => {
   it ("calculates x + y = z", () => {
-    expect(Add(10, 5)).toEqual(16);
+    expect(Add(10, 5)).toEqual(15);
+  });
+
+  it ("calculates x + y != z", () => {
+    expect(Add(10, 5)).not.toEqual(14);
   });
 });
