b9cd655f03b0be22b762b8889c30b4e5fe99d424
asolanky <alwinsolanky@gmail.com>
2018-01-14 18:10:58 +0000

    amended test to fail

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -2,6 +2,6 @@ var Add = require("../app");
 
 describe("Add functionality", () => {
   it ("calculates x + y = z", () => {
-    expect(Add(10, 5)).toEqual(15);
+    expect(Add(10, 5)).toEqual(16);
   });
 });
